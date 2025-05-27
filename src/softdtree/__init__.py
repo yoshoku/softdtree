fa068d98ba369a05b95ee0207d30becff4a01110
yoshoku <yoshoku@outlook.com>
2025-05-27 23:05:17 +0900

    docs: add package docstring

--- src/softdtree/__init__.py
+++ src/softdtree/__init__.py
@@ -1,3 +1,9 @@
+"""
+Soft Decision Tree for Classification and Regression.
+
+This module provides classifier and regressor based on soft decision tree.
+"""
+
 from ._softdtree import BaseSoftDecisionTreeClassifier, BaseSoftDecisionTreeRegressor, Node
 
 __all__ = [
